ad045962b1d5e226c33ba17318b0cd49ae6f8d26
SunnatDevPy <stalker.yoqubov@gmail.com>
2024-11-04 17:56:21 +0500

    add

--- bot/group.py
+++ bot/group.py
@@ -85,7 +85,7 @@ class AddAdmin(StatesGroup):
     user_id = State()
 
 
-@group_router.message(Command("words"))
+@group_router.message(Command("words@stock_security_bot"))
 async def add_bad_word(message: Message, bot: Bot):
     member = await bot.get_chat_member(message.chat.id, message.from_user.id)
     if member.status in ['administrator', 'creator']:
