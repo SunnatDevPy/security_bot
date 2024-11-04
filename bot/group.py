844c628fda1400125052ef82c6b2b2203a462db8
SunnatDevPy <stalker.yoqubov@gmail.com>
2024-11-04 18:02:14 +0500

    add

--- bot/group.py
+++ bot/group.py
@@ -37,54 +37,6 @@ async def send_welcome(message: Message, bot: Bot):
     await message.answer(text, reply_markup=await start())
 
 
-@group_router.message(F.new_chat_member)
-async def on_bot_added_to_group(message: Message, bot: Bot):
-    if int(message.new_chat_member['id']) == int(bot.id):
-        await bot.send_message(
-            message.chat.id,
-            f"Bot guruxga qo'shildi: {message.chat.title} (Gurux ID: {message.chat.id})"
-        )
-
-
-@group_router.message(F.chat.type.in_(['group', 'supergroup']))
-async def filter_message(message: Message, bot: Bot):
-    user = await User.get(message.from_user.id)
-    if user == None:
-        from_user = message.from_user
-        user = await User.create(id=message.from_user.id, username=from_user.username,
-                                 full_name=from_user.first_name)
-    else:
-        pass
-    if contains_link(message):
-        await message.delete()
-        await message.answer(f"{message.from_user.first_name}, link tashlamang!")
-        await User.update(user.id, count=user.count + 1)
-
-    elif await contains_bad_word(message):
-        await message.delete()
-        await message.answer(f"{message.from_user.first_name}, haqoratli so'zlar yozmang!")
-        await User.update(user.id, count=user.count + 1)
-
-    if message.sticker:
-        await message.delete()
-        await message.answer(f"{message.from_user.first_name}, sticker tashlamang!")
-
-    user: User = await User.get(message.from_user.id)
-    if user.count == 3:
-        await bot.restrict_chat_member(
-            chat_id=message.chat.id,
-            user_id=user.id,
-            permissions=ChatPermissions(can_send_messages=True),
-            until_date=message.date + timedelta(seconds=15),
-            request_timeout=15
-        )
-        await User.update(message.from_user.id, count=0)
-
-
-class AddAdmin(StatesGroup):
-    user_id = State()
-
-
 @group_router.message(F.text.startswith('/words'))
 async def add_bad_word(message: Message, bot: Bot):
     print(message.text)
@@ -96,28 +48,13 @@ async def add_bad_word(message: Message, bot: Bot):
         await message.reply("Sizda xuquq yo'q.")
 
 
-@group_router.callback_query(F.data.startswith('words_'))
-async def delete_admins(call: CallbackQuery, state: FSMContext):
-    data = call.data.split('_')
-    await call.answer()
-    if data[1] == 'add':
-        await call.message.delete()
-        await state.set_state(AddAdmin.user_id)
-        await call.message.answer("Yangi so'z qo'shing")
-    elif data[1] == 'delete':
-        try:
-            await Words.delete(int(data[-1]))
-            await call.message.edit_text("So'zlar to'plami", reply_markup=await words())
-        except:
-            await call.message.answer('Xatolik yuz berdi')
-
-
-@group_router.message(AddAdmin.user_id)
-async def add_admin(call: Message, state: FSMContext):
-    await Words.create(text=call.text)
-    await call.answer("Yangi so'z qo'shildi")
-    await call.answer("So'zlar to'plami", reply_markup=await words())
-    await state.clear()
+@group_router.message(F.new_chat_member)
+async def on_bot_added_to_group(message: Message, bot: Bot):
+    if int(message.new_chat_member['id']) == int(bot.id):
+        await bot.send_message(
+            message.chat.id,
+            f"Bot guruxga qo'shildi: {message.chat.title} (Gurux ID: {message.chat.id})"
+        )
 
 
 @group_router.message(Command("ban"))
@@ -182,3 +119,66 @@ async def check_video_content(message: Message, bot: Bot):
     if any(keyword in video.file_path.lower() for keyword in UNWANTED_KEYWORDS):
         await message.reply("⚠️ Ushbu video nomaqbul kontentni o'z ichiga olishi mumkin!")
         await bot.unban_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+
+
+@group_router.message(F.chat.type.in_(['group', 'supergroup']))
+async def filter_message(message: Message, bot: Bot):
+    user = await User.get(message.from_user.id)
+    if user == None:
+        from_user = message.from_user
+        user = await User.create(id=message.from_user.id, username=from_user.username,
+                                 full_name=from_user.first_name)
+    else:
+        pass
+    if contains_link(message):
+        await message.delete()
+        await message.answer(f"{message.from_user.first_name}, link tashlamang!")
+        await User.update(user.id, count=user.count + 1)
+
+    elif await contains_bad_word(message):
+        await message.delete()
+        await message.answer(f"{message.from_user.first_name}, haqoratli so'zlar yozmang!")
+        await User.update(user.id, count=user.count + 1)
+
+    if message.sticker:
+        await message.delete()
+        await message.answer(f"{message.from_user.first_name}, sticker tashlamang!")
+
+    user: User = await User.get(message.from_user.id)
+    if user.count == 3:
+        await bot.restrict_chat_member(
+            chat_id=message.chat.id,
+            user_id=user.id,
+            permissions=ChatPermissions(can_send_messages=True),
+            until_date=message.date + timedelta(seconds=15),
+            request_timeout=15
+        )
+        await User.update(message.from_user.id, count=0)
+
+
+class AddAdmin(StatesGroup):
+    user_id = State()
+
+
+@group_router.callback_query(F.data.startswith('words_'))
+async def delete_admins(call: CallbackQuery, state: FSMContext):
+    data = call.data.split('_')
+    await call.answer()
+    if data[1] == 'add':
+        await call.message.delete()
+        await state.set_state(AddAdmin.user_id)
+        await call.message.answer("Yangi so'z qo'shing")
+    elif data[1] == 'delete':
+        try:
+            await Words.delete(int(data[-1]))
+            await call.message.edit_text("So'zlar to'plami", reply_markup=await words())
+        except:
+            await call.message.answer('Xatolik yuz berdi')
+
+
+@group_router.message(AddAdmin.user_id)
+async def add_admin(call: Message, state: FSMContext):
+    await Words.create(text=call.text)
+    await call.answer("Yangi so'z qo'shildi")
+    await call.answer("So'zlar to'plami", reply_markup=await words())
+    await state.clear()
